9c62e2b6a46125bade21714629bffaff2d2dde63
jayceeth <75772603+jayceeth@users.noreply.github.com>
2021-03-30 22:08:20 -0700

    Added and styled the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Weather from "./Weather";
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
       <div className = "container">
         <p>
-          Weather App
-          <button>Search</button>
+          <Weather />
+          <SearchEngine />
         </p>
        </div>
     </div>
